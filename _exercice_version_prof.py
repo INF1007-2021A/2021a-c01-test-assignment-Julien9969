ab48563400802332e5ff8daf1499ff3c35412e2d
Julien9969 <julien.roux404@gmail.com>
2021-09-02 15:15:57 -0400

    Update _exercice_version_prof.py

--- _exercice_version_prof.py
+++ _exercice_version_prof.py
@@ -3,4 +3,8 @@
 
 if __name__ == '__main__':
     #TODO: Modifier le code ici
+<<<<<<< Updated upstream
     print("Le cours INF1007 huhuhuh")
+=======
+    print("Le cours INF1007rrrr")
+>>>>>>> Stashed changes
